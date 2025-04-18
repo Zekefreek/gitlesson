411bdd88c68820bfbe51ab035e8434eeb862d7e1
Ezekiel <ezekielaina93@gmail.com>
2025-04-18 10:55:11 +0100

    ssg python file

---
+++ ssg_firstclass.py
@@ -0,0 +1,13 @@
+print(type(2))
+print(5+4)
+print(5+4*2)
+list=[1,2,3,4,5,6,7,8,9]
+print(len(list))
+arr ={1,2,3,4}
+length_of_list =len(arr)
+print(length_of_list)
+firstName="Ezekiel "
+lastName="Aina"
+fullName =firstName+ "  "+lastName 
+print(fullName)
+print(3*firstName)
